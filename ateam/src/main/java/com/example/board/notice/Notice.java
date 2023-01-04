5aa78f05658d55d849c24cc33d9b7fd28e359377
masterpark81 <masterpark81@gmail.com>
2023-01-04 17:07:00 +0900

    공지사항 최종완료_진배_230104

--- ateam/src/main/java/com/example/board/notice/Notice.java
+++ ateam/src/main/java/com/example/board/notice/Notice.java
@@ -1,12 +1,23 @@
 package com.example.board.notice;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import org.springframework.stereotype.Controller;
+
+import com.example.board.qna.answer.Answer;
+import com.example.board.user.SiteUser;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,15 +26,21 @@ import lombok.Setter;
 @Setter
 @Entity
 public class Notice {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-    @Column(length = 200)
-    private String subject;
+	@Column(length = 200)
+	private String subject;
+	
+	@Column(columnDefinition="TEXT")
+	private String content;
+	
+	private LocalDateTime createDate;
 
-    @Column(columnDefinition = "TEXT")
-    private String content;
-
-    private LocalDateTime createDate;
+	@ManyToOne
+	private SiteUser author;
+	
+	private LocalDateTime modifyDate;
 }
--- ateam/src/main/java/com/example/board/notice/NoticeRepository.java
+++ ateam/src/main/java/com/example/board/notice/NoticeRepository.java
@@ -2,13 +2,14 @@ package com.example.board.notice;
 
 import java.util.List;
 
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface NoticeRepository extends JpaRepository<Notice, Integer> {
-    Notice findBySubject(String subject);
-    Notice findBySubjectAndContent(String subject, String content);
-    List<Notice> findBySubjectLike(String subject);
-    Page<Notice> findAll(Pageable pageable);
+		
 }
